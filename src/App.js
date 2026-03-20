c1bb5c05eb1155a9e6f1997ac97b2af55335a422
Gevorg Petrosyan <petrossian.gev@gmail.com>
2026-03-20 18:17:45 +0400

    adding 2 new routes for all users adn for each user

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import { PostLists } from './features/posts/PostsLists';
 import AddFormPost from './features/posts/AddPostForm';
 import SinglePostPage from './features/posts/SinglePostPage';
 import EditPostForm from './features/posts/EditPostForm';
+import { UserPage } from './features/users/UserPage';
+import { UsersList } from './features/users/UsersList';
 
 function App() {
   return (
@@ -29,6 +31,8 @@ function App() {
           />
           <Route path="/posts/:postId" component={SinglePostPage} />
           <Route path="/edit-post/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route path="/users/:userId" component={UserPage} />
           <Redirect to="/" />
         </Switch>
       </div>
